e2d8607a99e0596afc4a018ca00063730c47bc06
Maria <manja336@gmail.com>
2023-09-21 22:09:14 +0300

    work3_create_contacts

---
+++ ContactTestCase.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+class ContactTestCase(unittest.TestCase):
+    def setUp(self):
+        self.wd = webdriver.Firefox(executable_path=r'geckodriver.exe')
+        self.wd.implicitly_wait(30)
+
+    def test_contact_test_case(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, "admin", "secret")
+        self.open_page_add_contacts(wd)
+        self.create_contacts(wd, "Ivan", "Ivanovich", "Ivanov", "ivanushka", "test", "test", "000000fff", "87899877777",
+                             "5656787887", "test@gmail.com", "13", "November", "1994", "test")
+        self.return_to_home(wd)
+        self.logout(wd)
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+    def return_to_home(self, wd):
+        wd.find_element_by_link_text("home").click()
+    def create_contacts(self, wd, firstname, middlename, lastname, nickname, company, address, home, mobile, work,
+                        email, bday, bmonth, byear, address2):
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").send_keys(firstname)
+        wd.find_element_by_name("middlename").send_keys(middlename)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").send_keys(lastname)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").send_keys(nickname)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").send_keys(company)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").send_keys(address)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").send_keys(home)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").send_keys(mobile)
+        wd.find_element_by_name("work").click()
+        wd.find_element_by_name("work").send_keys(work)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").send_keys(email)
+        wd.find_element_by_name("email2").click()
+        wd.find_element_by_name("email2").send_keys("-")
+        wd.find_element_by_name("email3").click()
+        wd.find_element_by_name("bday").click()
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(bday)
+        wd.find_element_by_name("bmonth").click()
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(bmonth)
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").send_keys(byear)
+        wd.find_element_by_name("new_group").click()
+        Select(wd.find_element_by_name("new_group")).select_by_visible_text("group")
+        wd.find_element_by_name("address2").click()
+        wd.find_element_by_name("address2").clear()
+        wd.find_element_by_name("address2").send_keys(address2)
+        wd.find_element_by_name("phone2").click()
+        wd.find_element_by_name("phone2").click()
+        # submit contacts creation
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+    def open_page_add_contacts(self, wd):
+        wd.find_element_by_link_text("add new").click()
+
+    def login(self, wd, username, password):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
+
+    def is_element_present(self, how, what):
+        try: self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.wd.switch_to_alert()
+        except NoAlertPresentException as e: return False
+        return True
+
+    def tearDown(self):
+        self.wd.quit()
+
+if __name__ == "__main__":
+    unittest.main()
