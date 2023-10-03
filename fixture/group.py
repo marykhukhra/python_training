fdc468b24be94bb2444538d6dd78b5f6469edb4f
Maria <manja336@gmail.com>
2023-10-03 21:43:30 +0300

    Задание 8 редактирование

--- fixture/group.py
+++ fixture/group.py
@@ -29,11 +29,14 @@ class GroupHelper:
     def delete_first_group(self):
         wd = self.app.wd
         self.open_groups_page()
-        # select first group
-        wd.find_element_by_name("selected[]").click()
+ёё        self.select_first_group(wd)
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
+
+    def select_first_group(self, wd):
+        # select first group
+        wd.find_element_by_name("selected[]").click()
 
     def edit_group(self, new_header):
         wd = self.app.wd
@@ -43,6 +46,15 @@ class GroupHelper:
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_header").send_keys(new_header)
         wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+
+    def modify_first_group(self, new_group_data):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_first_group()
+        # open modification form
+        # fill group form
+        # submit modification
         self.return_to_groups_page()
 
     def return_to_groups_page(self):
