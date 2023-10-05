a3a8fc3700ced50f5ea973aaea43e86d271dcea8
Maria <manja336@gmail.com>
2023-10-05 13:36:03 +0300

    Исправление замечания к заданию 7. Перенос параметров контакта в класс

--- fixture/contact.py
+++ fixture/contact.py
@@ -3,54 +3,6 @@ import time
 from selenium.webdriver.support.select import Select
 
 
-def contact_parametr(contact, driver):
-    driver.find_element_by_name("firstname").click()
-    driver.find_element_by_name("firstname").send_keys(contact.firstname)
-    driver.find_element_by_name("middlename").send_keys(contact.middlename)
-    driver.find_element_by_name("lastname").click()
-    driver.find_element_by_name("lastname").send_keys(contact.lastname)
-    driver.find_element_by_name("nickname").click()
-    driver.find_element_by_name("nickname").send_keys(contact.nickname)
-    driver.find_element_by_name("company").click()
-    driver.find_element_by_name("company").send_keys(contact.company)
-    driver.find_element_by_name("address").click()
-    driver.find_element_by_name("address").send_keys(contact.address)
-    driver.find_element_by_name("home").click()
-    driver.find_element_by_name("home").send_keys(contact.home_phone)
-    driver.find_element_by_name("mobile").click()
-    driver.find_element_by_name("mobile").send_keys(contact.self_mobile)
-    driver.find_element_by_name("mobile").click()
-    driver.find_element_by_name("work").send_keys(contact.work_mobile)
-    driver.find_element_by_name("email").click()
-    driver.find_element_by_name("email").send_keys(contact.email)
-    driver.find_element_by_name("bday").click()
-    Select(driver.find_element_by_name("bday")).select_by_visible_text(contact.bday)
-    driver.find_element_by_name("bmonth").click()
-    Select(driver.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
-    driver.find_element_by_name("byear").click()
-    driver.find_element_by_name("byear").send_keys(contact.byear)
-
-
-def contact_parametr_delete(driver):
-    driver.find_element_by_name("firstname").click()
-    driver.find_element_by_name("firstname").clear()
-    driver.find_element_by_name("lastname").click()
-    driver.find_element_by_name("lastname").clear()
-    driver.find_element_by_name("middlename").clear()
-    driver.find_element_by_name("nickname").click()
-    driver.find_element_by_name("nickname").clear()
-    driver.find_element_by_name("company").click()
-    driver.find_element_by_name("company").clear()
-    driver.find_element_by_name("address").click()
-    driver.find_element_by_name("address").clear()
-    driver.find_element_by_name("home").click()
-    driver.find_element_by_name("home").clear()
-    driver.find_element_by_name("mobile").click()
-    driver.find_element_by_name("mobile").clear()
-    driver.find_element_by_name("email").click()
-    driver.find_element_by_name("email").clear()
-
-
 class ContactHelper:
 
     def __init__(self, app):
@@ -59,9 +11,67 @@ class ContactHelper:
     def create(self, contact):
         driver = self.app.wd
         self.open_page_add_contact()
-        contact_parametr(contact, driver)
+        self.contact_parametr(contact)
         # submit contact creation
         driver.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+        self.return_to_home()
+
+    def contact_parametr(self, contact):
+        driver = self.app.wd
+        driver.find_element_by_name("firstname").click()
+        driver.find_element_by_name("firstname").send_keys(contact.firstname)
+        driver.find_element_by_name("middlename").send_keys(contact.middlename)
+        driver.find_element_by_name("lastname").click()
+        driver.find_element_by_name("lastname").send_keys(contact.lastname)
+        driver.find_element_by_name("nickname").click()
+        driver.find_element_by_name("nickname").send_keys(contact.nickname)
+        driver.find_element_by_name("company").click()
+        driver.find_element_by_name("company").send_keys(contact.company)
+        driver.find_element_by_name("address").click()
+        driver.find_element_by_name("address").send_keys(contact.address)
+        driver.find_element_by_name("home").click()
+        driver.find_element_by_name("home").send_keys(contact.home_phone)
+        driver.find_element_by_name("mobile").click()
+        driver.find_element_by_name("mobile").send_keys(contact.self_mobile)
+        driver.find_element_by_name("mobile").click()
+        driver.find_element_by_name("work").send_keys(contact.work_mobile)
+        driver.find_element_by_name("email").click()
+        driver.find_element_by_name("email").send_keys(contact.email)
+        driver.find_element_by_name("bday").click()
+        Select(driver.find_element_by_name("bday")).select_by_visible_text(contact.bday)
+        driver.find_element_by_name("bmonth").click()
+        Select(driver.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
+        driver.find_element_by_name("byear").click()
+        driver.find_element_by_name("byear").send_keys(contact.byear)
+
+    def contact_parametr_delete(self, contact):
+        driver = self.app.wd
+        driver.find_element_by_name("firstname").click()
+        driver.find_element_by_name("firstname").clear()
+        driver.find_element_by_name("lastname").click()
+        driver.find_element_by_name("lastname").clear()
+        driver.find_element_by_name("middlename").clear()
+        driver.find_element_by_name("nickname").click()
+        driver.find_element_by_name("nickname").clear()
+        driver.find_element_by_name("company").click()
+        driver.find_element_by_name("company").clear()
+        driver.find_element_by_name("address").click()
+        driver.find_element_by_name("address").clear()
+        driver.find_element_by_name("home").click()
+        driver.find_element_by_name("home").clear()
+        driver.find_element_by_name("mobile").click()
+        driver.find_element_by_name("mobile").clear()
+        driver.find_element_by_name("email").click()
+        driver.find_element_by_name("email").clear()
+
+    def edit_contact(self, contact):
+        wd = self.app.wd
+        self.app.wd.find_element_by_link_text("home").click()
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_xpath("//img[@alt='Edit']").click()
+        self.contact_parametr_delete(wd)
+        self.contact_parametr(contact)
+        wd.find_element_by_name("update").click()
         self.return_to_home()
 
     def delete_contact(self):
@@ -70,16 +80,6 @@ class ContactHelper:
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         wd.switch_to.alert.accept()
-
-    def edit_contact(self, contact):
-        wd = self.app.wd
-        self.app.wd.find_element_by_link_text("home").click()
-        wd.find_element_by_name("selected[]").click()
-        wd.find_element_by_xpath("//img[@alt='Edit']").click()
-        contact_parametr_delete(wd)
-        contact_parametr(contact, wd)
-        wd.find_element_by_name("update").click()
-        self.return_to_home()
 
     def return_to_home(self):
         self.app.wd.find_element_by_link_text("home").click()
